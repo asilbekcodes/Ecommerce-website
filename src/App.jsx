1c56a3f5c84ab909224c30ffe3a5f3aa35d53788
Asilbek Codes <asilbeksuvonov43@gmail.com>
2024-08-02 23:50:13 +0500

    update

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Product from './pages/Product'
+import Header from './components/Header'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Hero from './components/Hero'
+import Card from './components/Card'
+import Carts from './components/Carts'
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/Hero' element={<Hero/>}/>
+        <Route path='/Card' element={<Card/>}/>
+        <Route path='/Product' element={<Product/>}/>
+        <Route path='/Carts' element={<Carts/>}/>
+      </Routes>
+    </React.Fragment>
+  )
+}
+
+export default App
